c9e29963bcfacff728437e9bda1f8da0ac0b06fa
Toan Le <thanhtoanhcm1602@gmai.com>
2023-08-07 18:31:01 +0700

    - react_native_props.

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PresentationalComponent from './PresentationalComponent';
 
 export default class App extends React.Component {
   state = {
     myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   }
 
+  updateState = () => {
+    this.setState({ myState: 'The state is updated' })
+  }
+
   render() {
     return (
       <View>
-        <Text>{this.state.myState}</Text>
+        <PresentationalComponent myState={this.state.myState} updateState={this.updateState} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
