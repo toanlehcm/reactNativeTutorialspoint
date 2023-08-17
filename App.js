ba380616484cb2fdcd3c789f656641605589a5be
Toan Le <thanhtoanhcm1602@gmai.com>
2023-08-17 12:26:13 +0700

    - Add style for model 3D.

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import '@google/model-viewer';
+import Styles from './public/style/style.js';
 
 class App extends Component {
   render() {
-    return (
-      <View>
-        <Text>App</Text>
+    const MODEL_3D_TOWN_ON_CAPITAL_ISLE = require('./public/model3D/issum_the_town_on_capital_isle.glb');
 
-        <model-viewer alt="Neil Arms" src="https://diorama-3d.vercel.app/little_tokyo.glb" ar
+    const ENVIRONMENT_TEST = require('./public/model3D/environment_test.hdr')
+
+    const MODEL_3D_HOUSE = require('./public/model3D/little_tokyo.glb');
+
+    return (
+      <View style={Styles.model3DContainer}>
+        {/* <Text>App</Text> */}
+
+        {/* <model-viewer alt="Neil Arms" src="https://diorama-3d.vercel.app/little_tokyo.glb" ar
           environment-image="shared-assets/environments/moon_1k.hdr" poster="shared-assets/models/NeilArmstrong.webp"
           shadow-intensity="1" camera-controls touch-action="pan-y">
+        </model-viewer> */}
+
+        {/* <model-viewer alt="Neil Arms" src={MODEL_3D_HOUSE} ar
+          environment-image="shared-assets/environments/moon_1k.hdr" poster="shared-assets/models/NeilArmstrong.webp"
+          shadow-intensity="1" camera-controls touch-action="pan-y">
+        </model-viewer> */}
+
+        <model-viewer
+          id="model_3D_hero_title"
+          src={MODEL_3D_TOWN_ON_CAPITAL_ISLE}
+          style={Styles.model3DHeroTitle}
+          alt="model 3D hero title"
+          loading="lazy"
+          auto-rotate
+          reveal="auto"
+          shadow-intensity="1"
+          camera-controls
+          touch-action="pan-y"
+          environment-image={ENVIRONMENT_TEST}
+        // skybox-image={ENVIRONMENT_TEST}
+        >
         </model-viewer>
       </View>
     )
   }
 }
+
 export default App
---
+++ public/style/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+
+  model3DContainer: {
+    width: "100%",
+    height: "auto",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  model3DHeroTitle: {
+    width: 1000,
+    height: 500,
+    backgroundColor: "#1C1E3D",
+  }
+});
